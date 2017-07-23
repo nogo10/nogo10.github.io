830c8eda56354759033c34244d786539da9c1923
nogo10 <smichel_designer@hotmail.com>
2017-07-23 17:15:33 +0000

    update

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ var Web3 = require('web3');
 //Metamask stuff
 //https://github.com/MetaMask/faq/blob/master/DEVELOPERS.md
 window.addEventListener('load', function() {
-
+/*
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
     // Use Mist/MetaMask's provider
@@ -15,7 +15,7 @@ window.addEventListener('load', function() {
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
     window.web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/vXPxpEWW5GUNZ3CM6JBT"));
   }
-
+*/
   // Now you can start your app & access web3 freely:
   if (window.web3.isConnected) {
     console.log("hola");
