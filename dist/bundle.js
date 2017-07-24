573d7b4a1f7b6e080bfe26157fc8c119de933e3f
nogo10 <smichel_designer@hotmail.com>
2017-07-24 04:14:02 +0000

    update

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,23 +7,25 @@
 window.addEventListener('load', function() {
 
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
- /* if (typeof web3 !== 'undefined') {
-    // Use Mist/MetaMask's provider
-    window.web3 = new Web3(Web3.currentProvider);
-  }
-  else {
-    console.log('No web3? You should consider trying MetaMask!');
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/vXPxpEWW5GUNZ3CM6JBT"));
-  }*/
+  /* if (typeof web3 !== 'undefined') {
+     // Use Mist/MetaMask's provider
+     window.web3 = new Web3(Web3.currentProvider);
+   }
+   else {
+     console.log('No web3? You should consider trying MetaMask!');
+     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+     window.web3 = new Web3(new Web3.providers.HttpProvider("https://ropsten.infura.io/vXPxpEWW5GUNZ3CM6JBT"));
+   }*/
 
   // Now you can start your app & access web3 freely:
   if (window.web3.isConnected) {
-    console.log("hola");
+    console.log("Your browser connected to web3");
     console.log(web3.version);
-    console.log(web3.eth.getAccounts);
-    web3.net.peerCount( function(err, result){console.log(result) })}
-  
+    console.log(web3.eth.getAccounts());
+    var defaultAccount = web3.eth.defaultAccount;
+    console.log(defaultAccount);
+  }
+
 
   else {
     console.log("not connected to web3!!");
@@ -33,10 +35,10 @@ window.addEventListener('load', function() {
     if (error) {
       return error;
     }
-    
-     else {
-    console.log(result);
-  }
+
+    else {
+      console.log(result);
+    }
 
     var abi = [{
       "constant": true,
@@ -241,14 +243,16 @@ window.addEventListener('load', function() {
       "name": "Approval",
       "type": "event"
     }];
-    console.log(window.web3.version);
 
     /*        getBalance          */
     $(function() {
       $('#kbt').click(function(e) {
         e.preventDefault();
         var myaddress = document.getElementById("myinput").value;
-        document.getElementById("kbt").innerHTML.web3.eth.contract(abi[0]).at('0x284057ca9b16d0bbc8722601df1eede6e56a60d9').balanceOf(myaddress);
+        var MyContract = web3.eth.contract(abi);
+        var myContractInstance = MyContract.at('0x284057ca9b16d0bbc8722601df1eede6e56a60d9');
+        var result = myContractInstance.balanceOf(myaddress);
+        document.getElementById("kbt").innerHTML=result;
 
       });
     });
